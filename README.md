ca04723839a60b7876a473237003f1ddba731d10
dlscoccia <2054@holbertonschool.com>
2021-04-27 17:17:51 -0500

    adding fucntion to check input

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-readme
+# My title
+## My title2
+# My title3
+#### My title4
+### My title5
---
+++ markdown2html.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Python Module'''
+import sys
+args = sys.argv
+
+def checkFiles(args):
+    if (len(args) == 1):
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        exit(1)
+    elif (len(args) == 3):
+        try:
+            with open(args[1], 'r') as file:
+                readme = file.read()
+        except:
+            sys.stderr.write("Missing {}\n".format(args[1]))
+            exit(1)
+
+checkFiles(args);
+
